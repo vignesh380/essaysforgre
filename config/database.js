0bd272bb5f0d9b5654c6959b4e4b29da1185718c
Vignesh Shankar <vicky380@gmail.com>
2015-02-11 20:35:13 +0530

    removed all the database realed code

--- config/database.js
+++ config/database.js
@@ -9,16 +9,6 @@ function connect(connectionString) {
 	db.on('error',console.error.bind(console,'connection error :'));
 	db.once('open',function(callback){
 		console.log('Mongoose connected at: ', connectionString);
-		myObject = new myModel({name: 'Viggy'});
-   myObject.save(function (err,myObject){
-    if (err) {return console.error(err);
-    }
-    myObject.logger();
-  });
-  myModel.find(function(err,myObjects){
-    if (err) return console.error(err);
-    console.log(myObjects);
-  });
 	});
 }
 module.exports = connect;
