baecb19379e834a63fba346cae0dfb914d7e536d
Vignesh Shankar <vicky380@gmail.com>
2015-03-29 23:17:38 +0530

    (was bored) revamped the temp login and signup page to look decent

--- app/routes/signUpPageRoutes.js
+++ app/routes/signUpPageRoutes.js
@@ -7,7 +7,7 @@
 
 function signUpPage(req,res){
   console.log("got post request from login page");
-  res.sendFile('signup.html', { root: 'public' }); 
+  res.sendFile('signupTemp.html', { root: 'public' }); 
 }
 
 exports.signUpPage = signUpPage;
